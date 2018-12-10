ce66a333d322f0d55f1218faea0d2e821f8693bb
David Kane <d.r.kane@gmail.com>
2018-12-10 23:08:11 +0000

    change license description

--- setup.py
+++ setup.py
@@ -4,22 +4,19 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
     name='charity-base-client-python',
-    version='0.1.1',
+    version='0.1.2',
     description='A Python client library for interacting with the CharityBase REST API.',
     long_description=readme,
     long_description_content_type="text/markdown",
     author='David Kane',
     author_email='david@drkane.co.uk',
     url='https://github.com/drkane/charity-base-client-python',
-    license=license,
+    license="MIT",
     install_requires=requirements,
     packages=find_packages(exclude=('tests', 'docs')),
     setup_requires=["pytest-runner"],
