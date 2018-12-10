1a3a87e8360cccd53bd2514d834031c383d107e4
David Kane <d.r.kane@gmail.com>
2018-12-10 22:57:14 +0000

    Add package classifiers

--- setup.py
+++ setup.py
@@ -12,9 +12,10 @@ with open('requirements.txt') as f:
 
 setup(
     name='charity-base-client-python',
-    version='0.1.0',
+    version='0.1.1',
     description='A Python client library for interacting with the CharityBase REST API.',
     long_description=readme,
+    long_description_content_type="text/markdown",
     author='David Kane',
     author_email='david@drkane.co.uk',
     url='https://github.com/drkane/charity-base-client-python',
@@ -24,4 +25,9 @@ setup(
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
     python_requires='>=3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
